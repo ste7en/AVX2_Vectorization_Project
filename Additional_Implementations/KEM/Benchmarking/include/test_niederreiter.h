f53dbae45a96afca12edf541980a7e34839faca9
Stefano Formicola <stefano_17sf@libero.it>
2020-01-13 22:23:45 +0100

    number of tests = 100

--- Additional_Implementations/KEM/Benchmarking/include/test_niederreiter.h
+++ Additional_Implementations/KEM/Benchmarking/include/test_niederreiter.h
@@ -4,7 +4,7 @@
 #include "niederreiter.h"
 
 
-#define NUM_TESTS 30
+#define NUM_TESTS 100
 void print_KEM_parameters(void);
 void test_KEM_niederreiter_code(int ac, char *av[], long unsigned int NumTests);
 
